7c9276238f110086742bc191e959ccc0fe32cf6c
Emily Morgenbesser <emilymorgenbesser@Emilys-MacBook-Pro.local>
2021-08-17 09:52:27 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import bcrypt from 'bcryptjs'
+import  { Form, Button, Input } from 'antd'
+import 'antd/dist/antd.css'
 
-function App() {
+const mySalt = '$2a$10$vraQCCXIH/exwgd0Fp4G8.'
+
+export default function App() {
+  function handleSignup ({ email, password }) {
+    const hashedPassword = bcrypt.hashSync(password, mySalt)
+    fetch('https://auth-hash-api-ekm.web.app/signup', {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ email, password: hashedPassword })
+    })
+    .then(response => response.json())
+    .then(data => alert(data.message))
+    .catch(err => alert(err))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Form
+    labelCol={{ span: 8 }}
+    wrapperCol={{ span: 16 }}
+    onFinish={handleSignup}
+   >
+     <h1>Sign up</h1>
+     <Form.Item
+    label="Email"
+    name="email"
+    rules={[{ required: true, message: 'Please input your email!'}]}
+>
+       <Input />
+     </Form.Item>
+     <Form.Item
+    label="Password"
+    name="password"
+    rules={[{ required: true, message: 'Please input your password!'}]}
+>
+       <Input.Password />
+     </Form.Item>
+     <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+       <Button type='primary' htmlType="Submit">Sign up</Button>
+     </Form.Item>
+   </Form>
+   
+  )
 }
 
-export default App;
